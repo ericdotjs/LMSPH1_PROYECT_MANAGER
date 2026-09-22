bee0987757a5d5511752ec15f784533fc4963e69
Eric <ericmontalvo44@gmail.com>
2026-09-21 21:55:31 -0600

    New project window

---
+++ Views/NewProject.axaml.cs
@@ -0,0 +1,16 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LMSPH1_PROYECT_MANAGER.Views;
+
+public partial class NewProject : Window
+{
+    public NewProject()
+    {
+        
+    }
+
+
+}
